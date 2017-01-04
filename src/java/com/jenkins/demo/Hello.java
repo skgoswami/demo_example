65b21320559d5ed05a6799e8fea8665df3928f2e
skgoswami <s.kumar.goswami@accenture.com>
2017-01-04 22:31:37 +0530

    added web apps

---
+++ src/java/com/jenkins/demo/Hello.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jenkins.demo;
+
+/**
+ *
+ * @author s.kumar.goswami
+ */
+public class Hello {
+    
+    public void Hello(){
+        System.out.println("Hello Shailendra");
+    }
+    
+}
